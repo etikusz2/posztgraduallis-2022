b83b45f767a917a153af78356d1cca90c20982f8
Birtalan Etele <etele.birtalan@gmail.com>
2022-10-23 12:48:19 +0300

    Fej_6/Feladat_1

---
+++ 01_szamitogepesaritmetika/muveletek/x01_muveletek/feladatok/Fej_06/Feladat_1/feladat_1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Kérjen be egy számot és írja ki, hogy helló annyiszor.
+        // (Emlékezzünk a régi szenvedős megoldásra és értékeljük a haladást.)
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjon meg egy szamot: ");
+        int iMegadottSzam = sc.nextInt();
+        sc.nextLine();
+        int i;
+        for (i = 0; i <= iMegadottSzam; ++i) {
+            System.out.println("Hello ");
+        }
+    }
+}
