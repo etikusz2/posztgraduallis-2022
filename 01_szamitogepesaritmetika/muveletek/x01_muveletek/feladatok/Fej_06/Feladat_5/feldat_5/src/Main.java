4751c272d12fd3fce7f38911f896e7220c5e500a
Birtalan Etele <etele.birtalan@gmail.com>
2022-10-26 19:49:48 +0300

    6/5

---
+++ 01_szamitogepesaritmetika/muveletek/x01_muveletek/feladatok/Fej_06/Feladat_5/feldat_5/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Írjon programot ami addig olvas be karakterláncokat ameddig azt nem adjuk meg neki, hogy Exit.
+        // Minden beolvasott karakterláncot írjon is ki azonnal fordítva, de az Exit-et már ne.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjon meg egy karakterlancot: ");
+        String sKarakterlanc = sc.nextLine();
+        String sKarakterlancForditva = "";
+        while (sKarakterlanc.compareTo("Exit") != 0) {
+            for (int i = 0; i < sKarakterlanc.length(); i++) {
+                sKarakterlancForditva = sKarakterlanc.charAt(i) + sKarakterlancForditva;
+            }
+            System.out.println(sKarakterlancForditva);
+            System.out.println("Adjon meg egy karakterlancot: ");
+            sKarakterlanc = sc.nextLine();
+        }
+    }
+}
