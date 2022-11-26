a8314dcc61471ae33b8b16f5473a26265a3461bd
Birtalan Etele <etele.birtalan@gmail.com>
2022-11-26 15:35:29 +0200

    02//4/2

---
+++ 02_algoritmusok/04_Rekurzio/feladat_2/Feladat_02/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    // Írjon  ki  a  képernyőre  n  darab  csillagot.
+    // Oldja  meg  kétféleképpen  is.  Iteratív  módon  és  rekurzíó felhasználásával.
+    public static void main(String[] args) {
+        // iterativan
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        // rekurzivan
+        CsillagozasNszer(n);
+    }
+
+    private static String CsillagozasNszer(int n) {
+        if (n <= 0)
+            return "";
+
+        String CsillagozasN_1szer = CsillagozasNszer(n - 1);
+        System.out.print("*");
+        return "*";
+    }
+}
