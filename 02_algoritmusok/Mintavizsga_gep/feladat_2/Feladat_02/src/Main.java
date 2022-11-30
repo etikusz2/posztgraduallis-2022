cb7f8723f4e50c7c7417bc499d1ea1420978b61c
Birtalan Etele <etele.birtalan@gmail.com>
2022-11-30 21:10:15 +0200

    mintavizsga_x02/gep_2

---
+++ 02_algoritmusok/Mintavizsga_gep/feladat_2/Feladat_02/src/Main.java
@@ -0,0 +1,50 @@
+import java.text.DecimalFormat;
+
+public class Main {
+
+    /**
+     * A túraösvény teljes hossza a csúcsig.
+     */
+    private static double MaxDistance = 8500;
+
+    /**
+     * A csúcs magassága.
+     */
+    private static double MaxHeight = 1221;
+
+    public static void main(String[] args) {
+        // van egy szép kilátó 890 méteren
+        double targetHeight = 890;
+
+        // sétatávolság a kilátóig
+        double distanceToPanorama = calculateDistanceToHeight(targetHeight);
+        DecimalFormat df = new DecimalFormat("#");
+        System.out.println("Distace to the panorama is " + df.format(distanceToPanorama) + " meter.");
+
+    }
+
+    /**
+     * Mekkora távolságot kell megtenni ahhoz, hogy egy adott magasságot elérjünk?
+     *
+     * @param targetHeight Az elérendő magasság.
+     * @return Az ösvényen megtett út hossza amíg elérjük a kívánt magasságot.
+     */
+    private static double calculateDistanceToHeight(double targetHeight) {
+        double walkingDistance = MaxDistance * targetHeight / MaxHeight;
+        return walkingDistance;
+    }
+
+    /**
+     * Megadja, hogy az ösvényen megtett adott sétatávolság után mennyi szintkülönbséget
+     * tesz meg a túrázó (méterben).
+     *
+     * @param walkingDistance Az ösvény mentén lesétált távolság méterben.
+     */
+    private static double getAltitudeReachedAfterWalking(double walkingDistance) {
+        double sineScale = 500;
+        return ((Math.exp(walkingDistance / 1000) / (1 + Math.exp(walkingDistance / 1000)) - 0.5)
+                + ((Math.sin(walkingDistance / sineScale) + walkingDistance / sineScale)) / MaxDistance * sineScale)
+                / ((Math.exp(MaxDistance / 1000) / (1 + Math.exp(MaxDistance / 1000)) - 0.5)
+                + ((Math.sin(MaxDistance / sineScale) + MaxDistance / sineScale)) / MaxDistance * sineScale) * MaxHeight;
+    }
+}
