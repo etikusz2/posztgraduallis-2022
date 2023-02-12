f984e131cd6461407f18e82c037d60ee45143c68
Birtalan Etele <etele.birtalan@gmail.com>
2023-02-12 10:36:20 +0200

    04//FirstExamplesBox/2

---
+++ 04_OOP/Feladatsor_1/SecondExample/src/SecondExample.java
@@ -0,0 +1,20 @@
+public class SecondExample {
+    // Írjunk programot, amely kiírja a konzolra a parancssor argumentumait, a kisbetűket nagybetűkbe,
+    // a nagybetűket kisbetűkbe alakítva.
+    // Útmutatás: egy String objektum esetében egy adott karaktert a charAt(index) metódus segítségével kérdezhetünk le,
+    // az ellenőrzés és átalakítás a Character osztály statikus metódusainak segítségével történhet.
+
+    public static void main(String args[]) {
+        for (int i = 0; i < args.length; i++) {
+            for (int j = 0; j < args[i].length(); j++) {
+                if (Character.isUpperCase(args[i].charAt(j))) {
+                    System.out.print(Character.toLowerCase(args[i].charAt(j)));
+                } else {
+                    System.out.print(Character.toUpperCase(args[i].charAt(j)));
+                }
+            }
+            System.out.print(" ");
+        }
+
+    }
+}
