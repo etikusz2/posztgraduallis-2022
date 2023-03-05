1768cca25abddce30b3c52a588c87c132229df5a
Birtalan Etele <etele.birtalan@gmail.com>
2023-03-05 22:46:32 +0200

    04//ExamplesBox02/7 update

--- 04_OOP/ExamplesBox_02/SeventhExample/src/JShapeFrame.java
+++ 04_OOP/ExamplesBox_02/SeventhExample/src/JShapeFrame.java
@@ -43,10 +43,5 @@ public class JShapeFrame extends JFrame {
             }
         });
     }
-    public static void main(String[] args){
-        JShapeFrame shapeFrame = new JShapeFrame();
-        shapeFrame.setBounds(10, 10, 1024, 768);
-        shapeFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        shapeFrame.setVisible(true);
-    }
+
 }
